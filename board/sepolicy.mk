4f45251f964878040ca82016bd0e5467d4d6ae20
Prateek Chaubey <chaubeyprateek@gmail.com>
2017-09-25 17:00:17 +0000

    msm8937-common: Permissive

--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,5 +1,5 @@
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    $(VENDOR_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    $(VENDOR_PATH)/sepolicy
 
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
