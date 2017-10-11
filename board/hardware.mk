bfb8403eb468641640798e923050354390d2c531
Bruno Martins <bgcngm@gmail.com>
2017-10-12 00:04:11 +0100

    msm8937-common: overlay-lineage: Apply LineageOS rebrand
    Change-Id: Idc537612efe4cf870d6a8164bd04844c1bb40b2b
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,5 +1,3 @@
-# CM Hardware
-BOARD_USES_CYANOGEN_HARDWARE := true
+# LineageOS Hardware
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    device/xiaomi/land/cmhw
+    device/xiaomi/msm8937-common/lineagehw
