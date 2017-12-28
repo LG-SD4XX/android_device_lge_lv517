7bdb2de36cb1540658d95492a72f9b5708d6c352
Mikhail Naganov <mnaganov@google.com>
2017-12-28 13:29:05 -0800

    rolex: Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Test: "ok google" from locked screen
    Change-Id: I256bae45bc3c68febe5d7bb4bdbd559d752b7d96

--- product/audio.mk
+++ product/audio.mk
@@ -20,7 +20,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.1-impl
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
