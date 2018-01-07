5f6d8e7869d0b3b31c456f7e1f2a1e19ae95dbb4
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-07 13:53:04 -0800

    msm8937-common: Add QTI low power whitelist
    Change-Id: Idb4a954979f916a7903ead982bf18b33e5cf1baf
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -57,3 +57,7 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     libandroid_net \
     netutils-wrapper-1.0
+
+# Whitelisted app
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
