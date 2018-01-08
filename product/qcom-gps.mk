65a0af0c8565d61f5fd8aaf423b7bad7179e48d8
Bruno Martins <bgcngm@gmail.com>
2018-01-08 00:06:46 +0000

    msm8937-common: Update GPS stack to O
     * From tissot 8.0.0-OPR1.170623.026-7.12.29
    
    Change-Id: Id4f7249149ed6fc192c89cc44eef4c115abc7a05

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,20 +1,19 @@
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-impl-qti \
     gps.msm8937 \
     libcurl \
+    libgnss \
     libgnsspps
 
-PRODUCT_BOOT_JARS += \
-    com.qti.location.sdk
-
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(VENDOR_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-    $(VENDOR_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(VENDOR_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
-    $(VENDOR_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
-    $(VENDOR_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+    $(VENDOR_PATH)/gps/etc/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(VENDOR_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(VENDOR_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(VENDOR_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(VENDOR_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(VENDOR_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(VENDOR_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # Permissions
 PRODUCT_COPY_FILES += \
