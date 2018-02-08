d6aa1fa3f562b140fc16c4aa69e094b8a0de0753
TheScarastic <warabhishek@gmail.com>
2018-02-08 16:11:48 +0530

    rolex: Move camera configs to vendor
     * Also sed sensor module to load them from vendor while extracting it
     * And update the bloblist
    
    Change-Id: Id200fb415d47556224412082210fefc7dba72165

--- extract-files.sh
+++ extract-files.sh
@@ -23,3 +23,7 @@ export VENDOR=xiaomi
 export DEVICE_COMMON=msm8917-common
 
 ./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+
+# Hax for camera configs
+CAMERA2_SENSOR_MODULES="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
+sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
