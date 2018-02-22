8e8d615282e9bdb0b78199ba00d9432dd63079f0
Anton Hansson <hansson@google.com>
2018-02-22 16:12:29 +0000

    rolex: Set LOCAL_SDK_VERSION where possible
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant packages
    Bug: 73535841
    Change-Id: Ic906adbd670c4dbfe2cdf0b71cdbeaf9ac08a27e

--- doze/Android.mk
+++ doze/Android.mk
@@ -7,8 +7,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := XiaomiDoze
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_SDK_VERSION := current
 
 LOCAL_USE_AAPT2 := true
 
