f20cd37c33c8d89d8cb6d00560056037a54860a6
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2018-04-17 08:54:18 +0200

    rolex: Add DT2W
    Change-Id: I1b263264b2eb9b182fa36ae7665131f4fb60faff

---
+++ rootdir/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+# Device specific init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.rolex.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.rolex.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
