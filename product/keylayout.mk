90c793634f6c0c0ee53ca7bec56a59eacf00d760
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2018-04-18 08:37:04 +0200

    msm8937-common: Import more keylayouts
     * Needed by Rolex and Riva
    
    Change-Id: I5da42e40aaca1d8daaa6ef866429f21df1604284

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,4 +1,6 @@
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/keylayout/ft5x06_720p.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x06_720p.kl \
+    $(VENDOR_PATH)/keylayout/ft5346.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5346.kl \
+    $(VENDOR_PATH)/keylayout/goodix-ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix-ts.kl \
     $(VENDOR_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
