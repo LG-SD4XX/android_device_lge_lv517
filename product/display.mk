1163b9311574256c1254fd0c501013db28f9c8ee
Schodowski24 <alrakhesyafauzan@gmail.com>
2018-04-30 12:40:54 +0200

    msm8917-common: Add additional display properties
     * To improve overall smoothness
     * A special thanks to @AyraHikari (Telegram) and @TambahRam (Telegram)
    
    Change-Id: I5c2a8481102f0b0db3deecbe020ad8a4a6e61d69

--- product/display.mk
+++ product/display.mk
@@ -48,15 +48,27 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=1 \
     debug.enable.sglscale=1 \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.mdpcomp.logs=0 \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.hw=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.recomputecrop=0 \
     dev.pm.dyn_samplingrate=1 \
+    persist.debug.wfd.enable=1 \
     persist.demo.hdmirotationlock=false \
     persist.hwc.enable_vds=1 \
     persist.hwc.mdpcomp.enable=true \
+    persist.sys.wfd.virtual=0 \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=320
+    ro.qualcomm.cabl=0 \
+    ro.sf.lcd_density=320 \
+    sdm.debug.disable_rotator_split=1 \
+    sdm.debug.disable_skip_validate=1 \
+    sdm.perf_hint_window=50 \
+    sys.display-size=3840x2160
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
