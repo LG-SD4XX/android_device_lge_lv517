5775108ac8a6bc1dd36051364d9b7507c2143de5
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2018-05-09 08:50:35 +0200

    msm8917-common: Commonize OSS camera HAL
     * Rolex and Riva can use the same OSS HAL, so lets commonize it.
    
    Change-Id: Iba1ced0c1c4826673ee544d0a78843365ed8ed71

--- product/camera.mk
+++ product/camera.mk
@@ -5,6 +5,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8937 \
+    libmm-qcamera
+	
 # Camera HAL
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
@@ -22,6 +27,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera.display.umax=1920x1080 \
     camera.display.lmax=1280x720 \
     camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
+    media.camera.ts.monotonic=1 \
     persist.camera.gyro.android=1 \
     persist.camera.HAL3.enabled=0 \
     persist.camera.is_type=1 \
