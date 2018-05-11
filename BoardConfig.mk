47c330c3f6a4ca412414d5cd3da2556a60e39514
deadman96385 <seanhoyt963@gmail.com>
2018-05-11 16:01:22 -0500

    Add proper double tap to wake node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 BOARD_NFC_CHIPSET := pn548
 BOARD_NFC_DEVICE := "/dev/pn547"
 
+# Tap to wake
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap_to_wake"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 
