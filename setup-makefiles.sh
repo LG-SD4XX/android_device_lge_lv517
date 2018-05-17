2b910c0805fe80150d1741d199b51742d7f0f568
deadman96385 <seanhoyt963@gmail.com>
2018-05-17 16:29:18 -0500

    Updat extract scripts to new format
    It now is all in the common tree

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -18,32 +18,8 @@
 set -e
 
 # Required!
-DEVICE=ph2n
-VENDOR=lge
+export DEVICE=ph2n
+export DEVICE_COMMON=sd4xx-common
+export VENDOR=lge
 
-INITIAL_COPYRIGHT_YEAR=2017
-
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-# The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# We are done!
-write_footers
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
