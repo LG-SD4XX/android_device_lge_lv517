53601d9d9d247124954b3649eed787142c021e4d
travisholt92 <Travisholt92@gmail.com>
2018-05-19 17:53:37 -0400

    remove link to device/lge/common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,4 +42,3 @@ TARGET_SYSTEM_PROP += $(LOCAL_PATH)/system.prop
 
 # inherit from the proprietary version
 -include vendor/lge/lv517/BoardConfigVendor.mk
--include device/lge/common/BoardConfigCommon.mk
