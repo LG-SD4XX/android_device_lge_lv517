f209ab37d3bd581a890634ff102b646b4d4d5180
TheStrechh <TheStrechh@gmail.com>
2018-06-10 01:49:12 +0000

    rolex: Remove "debug.sf.enable_hwc_vds" property
     * This prop breaks screen recording on msm8917 targets
    
    Change-Id: I731a30a7cfd3835a97425165b0392592fcaf2b29

--- product/display.mk
+++ product/display.mk
@@ -52,7 +52,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.enable.sglscale=1 \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.mdpcomp.logs=0 \
-    debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
