ba8c6cb232174f366b5ca3628fe70ba1f18212b2
Ritesh Saxena <riteshsax007@gmail.com>
2018-07-17 16:48:07 +0000

    rolex: Build vendor version of hidl shims
    Change-Id: Ic1a95231564626cde8f5a9f5f6f8bad8cd0159a0

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,5 +1,7 @@
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_vendor \
     android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_vendor \
     android.hidl.manager@1.0-java
