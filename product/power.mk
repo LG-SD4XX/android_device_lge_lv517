dfde565e787f791ffc04eafa3704d2eb5fe209d4
travisholt92 <Travisholt92@gmail.com>
2018-12-04 01:17:37 -0600

    Remove init for now
    Will revist once this is a common tree again
    
    Also remove riva power profile

--- product/power.mk
+++ product/power.mk
@@ -1,6 +1,3 @@
 # Power IMPL
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service-qti
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power_profile_lv517.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_profile_lv517.xml
