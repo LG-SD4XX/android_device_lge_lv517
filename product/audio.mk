cc4a216be9b57993d49d4d62716aed5db649c109
travisholt92 <Travisholt92@gmail.com>
2019-02-07 13:21:46 -0500

    [LV517] Fix a typo with mixer_paths.xml

--- product/audio.mk
+++ product/audio.mk
@@ -28,7 +28,7 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
 	$(DEVICE_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
 	$(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-	$(DEVICE_PATH)/audio/mixer_paths.xml:/system/etc/mixer_paths.xml \
+	$(DEVICE_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
 	$(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
 	$(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
 	$(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
