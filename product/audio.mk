1673800be1d17ce60ee0f13f67f8cc1e47d2471a
travisholt92 <Travisholt92@gmail.com>
2019-04-08 17:28:52 -0400

    Build newer audio services for Pie

--- product/audio.mk
+++ product/audio.mk
@@ -16,11 +16,11 @@ PRODUCT_PACKAGES += \
 
 # Audio HAL
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.1-impl
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
