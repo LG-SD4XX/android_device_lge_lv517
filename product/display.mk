68554706eca7438532015df9d82caf789e6cd610
travisholt92 <Travisholt92@gmail.com>
2019-04-08 17:30:22 -0400

    Display service updates

--- product/display.mk
+++ product/display.mk
@@ -27,12 +27,12 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libgenlock \
     memtrack.msm8937 \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.1_vendor \
 
 # Display calibration
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Display HAL
 PRODUCT_PACKAGES += \
@@ -43,6 +43,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.configstore@1.0-service \
 
 # Permissions
 PRODUCT_COPY_FILES += \
